161cf67630ee4d76800793b90cd9390abc6fddd4
William Meng <wlmeng11@gmail.com>
2013-04-07 10:38:04 -0700

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <opencv/core/core.hpp>
+#include <opencv/highgui/highgui.hpp>
+#include <opencv/imgproc/imgproc.hpp>
+
+using namespace cv;
+using namespace std;
+
+int main(int argc, char **argv) {
+}
