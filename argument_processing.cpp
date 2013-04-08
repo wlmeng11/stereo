a2f740bdc9260da7fd36e77c5e329c5dea3b05e6
William Meng <wlmeng11@gmail.com>
2013-04-07 21:08:04 -0700

    Implemented proper exit codes Replaced atoi with sscanf Combined structure declaration and initialization of elements in argument_processing.cpp (shorter, but degrades readability and flexibility). Also improved some formatting.

--- argument_processing.cpp
+++ argument_processing.cpp
@@ -1,20 +1,7 @@
 #include "argument_processing.hpp"
 
 struct arguments readArgs(int argc, char **argv) {
-	struct arguments arg;
-
-	arg.gui = 0;
-	arg.write = 0;
-	arg.host = 0;
-	arg.port = 0;
-	arg.cam = 0;
-	arg.file = 0;
-	arg.hostname = NULL;
-	arg.portno = -1;
-	arg.camera1 = -1;
-	arg.camera2 = -1;
-	arg.filename1 = NULL;
-	arg.filename2 = NULL;
+	struct arguments arg = { 0, 0, 0, 0, 0, 0, NULL, -1, -1, -1, NULL, NULL };
 
 	for (int i = 1; i < argc; i++) {
 		if ( (strcmp(argv[i], "-g") == 0) || 
@@ -31,8 +18,10 @@ struct arguments readArgs(int argc, char **argv) {
 				i++;
 				arg.hostname = argv[i];
 			}
-			else
+			else {
 				cerr << "Usage: -h <host IP>" << endl;
+				exit(1);
+			}
 		}
 		else if ( (strcmp(argv[i], "-p") == 0) ||
 				(strcmp(argv[i], "--port") == 0)) {
@@ -40,30 +29,38 @@ struct arguments readArgs(int argc, char **argv) {
 			if (i + 1 <= argc -1) {
 				i++;
 				sscanf(argv[i], "%d", &arg.portno);
-				//portno = atoi(argv[i]);
 			}
-			else
+			else {
 				cerr << "Usage: -p <port number>" << endl;
+				exit(1);
+			}
 		}
-	
 		else if ( (strcmp(argv[i], "-c") == 0) ||
 				(strcmp(argv[i], "--cam") == 0) ) {
 			arg.cam = 1;
-			if (i + 1 <= argc - 1) {
+			if (i + 2 <= argc - 1) {
 				i++;
-				arg.camera1 = atoi(argv[i]);
+				sscanf(argv[i], "%d", &arg.camera1);
 				i++;
-				arg.camera2 = atoi(argv[i]);
+				sscanf(argv[i], "%d", &arg.camera2);
+			}
+			else {
+				cerr << "Usage: -c <camera 1> <camera 2>" << endl;
+				exit(1);
 			}
 		}
 		else if ( (strcmp(argv[i], "-f") == 0) ||
 				(strcmp(argv[i], "--file") == 0) ) {
 			arg.file = 1;
-			if (i + 1 <= argc - 1) {
+			if (i + 2 <= argc - 1) {
 				i++;
 				arg.filename1 = argv[i];
 				i++;
 				arg.filename2 = argv[i];
+			}
+			else {
+				cerr << "Usage: -f <file 1> <file 2>" << endl;
+				exit(1);
 			}
 		}
 	}
--- main.cpp
+++ main.cpp
@@ -24,29 +24,35 @@ int main(int argc, char **argv) {
 
 	if (argc < 2) {
 		cerr << "Usage: " << argv[0] << " <options>" << endl;
-		return -1;
+		return 1;
 	}
 	else if (arg.port == 1 && arg.host == 0) {
 		cerr << "Please specify a hostname" << endl;
-		return -1;
+		return 1;
 	}
 	else if (arg.port == 0 && arg.host == 1) {
 		cerr << "Please specify a portnumber" << endl;
-		return -1;
+		return 1;
 	}
 	else if ( (arg.file == 0 && arg.cam == 0) ||
 			(arg.file == 1 && arg.cam == 1) ) {
 		cerr << "Please specify either 2 cameras or 2 media files" << endl;
-		return -1;
+		return 1;
 	}
 
 	// printf what we got from the arguments
-	if (arg.gui) cout << "Using GUI" << endl;
-	if (arg.write) cout << "Writing to file" << endl;
-	if (arg.host) cout << "Hostname: " << arg.hostname << endl;
-	if (arg.port) cout << "Port number: " << arg.portno << endl;
-	if (arg.cam) cout << "Using cameras: " << arg.camera1 << arg.camera2 << endl;
-	if (arg.file) cout << "Using files: " << arg.filename1 << arg.filename2 << endl;
+	if (arg.gui)
+		cout << "Using GUI" << endl;
+	if (arg.write)
+		cout << "Writing to file" << endl;
+	if (arg.host)
+		cout << "Hostname: " << arg.hostname << endl;
+	if (arg.port)
+		cout << "Port number: " << arg.portno << endl;
+	if (arg.cam)
+		cout << "Using cameras: " << arg.camera1 << arg.camera2 << endl;
+	if (arg.file)
+		cout << "Using files: " << arg.filename1 << arg.filename2 << endl;
 	
 	// connect to server if specified
 	if (arg.host == 0)
@@ -55,6 +61,7 @@ int main(int argc, char **argv) {
 		if ( server_connect(arg.hostname, arg.portno) ) {
 			server_disconnect();
 			cerr << "Couldn't connect to server" << endl;
+			return 1;
 		}
 	}
 
@@ -70,8 +77,10 @@ int main(int argc, char **argv) {
 		capture2.open(arg.filename2);
 		rate1 = capture1.get(CV_CAP_PROP_FPS);
 		rate2 = capture2.get(CV_CAP_PROP_FPS);
-		if (rate1 != rate2)
-			cerr << "Mismatching framerates!\n";
+		if (rate1 != rate2) {
+			cerr << "Mismatching framerates!" << endl;
+			return 1;
+		}
 	}
 
 	return 0;
