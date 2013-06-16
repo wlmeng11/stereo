8440aca420d96d9318f70bce3369b17b11afa1d1
William Meng <wlmeng11@gmail.com>
2013-06-15 18:42:23 -0700

    Forgot to add server.c before

---
+++ server.c
@@ -0,0 +1,19 @@
+#include "server.h"
+
+int server_start (int portno) {
+	printf("Starting server on port %d\n", portno);
+
+	return 0;
+}
+
+int server_accept () {
+
+	// fork process and accept
+
+	return 0;
+}
+
+int server_close () {
+
+	return 0;
+}
