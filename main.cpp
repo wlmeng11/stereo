e3bd584a5df5ec3c972a740f6293ff1309e0539c
William Meng <wlmeng11@gmail.com>
2013-06-15 23:03:02 -0700

    Read video sequences

--- main.cpp
+++ main.cpp
@@ -14,11 +14,13 @@
 using namespace cv;
 using namespace std;
 
+void CombineImages (const Mat &left, const Mat &right, Mat &combined); // combines left and right images into one big image
+
 int main(int argc, char **argv) {
 	VideoCapture capture1, capture2;
 	double rate1, rate2;
 
-	float distance;
+	//float distance;
 
 	struct parameters pars;
 
@@ -60,5 +62,53 @@ int main(int argc, char **argv) {
 		}
 	}
 
+	if (!capture1.isOpened()) {
+		cerr << "Failed to open capture 1" << endl;
+		return EX_NOINPUT;
+	}
+	else if (!capture2.isOpened()) {
+		cerr << "Failed to open capture 2" << endl;
+		//return EX_NOINPUT;
+	}
+
+	cout << "Left framerate: " << rate1 << endl;
+	cout << "Right framerate: " << rate2 << endl;
+
+	Mat left, right;
+	Mat combined;
+
+	int delay = 1000/rate1;
+
+	for(int i = 0;;i++) { // use i to count frames
+		if (!capture1.read(left) ) break;
+		else if (!capture2.read(right) ) break;
+
+		CombineImages(left, right, combined);
+
+		if (pars.gui) {
+			imshow("Left", left);
+			//imshow("Right", right);
+			//imshow("Combined frames", combined);
+		}
+
+		if (capture1.get(CV_CAP_PROP_FRAME_COUNT) == 1)
+			// individual image instead of sequential stream
+			waitKey(0);
+		else if (waitKey(delay)>=0) break; // wait for delay until next frame or break if keystroke
+	}
+
 	return 0;
+}
+
+// this needs to go in a class in another file eventually
+void CombineImages (const Mat &left, const Mat &right, Mat &combined) {
+	cout << "Left height: " << left.rows << endl;
+	cout << "Left width: " << left.cols << endl;
+	cout << "Right height: " << right.rows << endl;
+	cout << "Right width: " << right.cols << endl;
+	//combined image doesn't exist yet
+	cout << "Combined height: " << combined.rows << endl;
+	cout << "Combined width: " << combined.cols << endl;
+
+	Mat imageROI; // region of interest
 }
